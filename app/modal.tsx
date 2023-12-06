d47720be72ec568f27c2d0d8922410d438f7bc43
stere <chaika.valeriia@chnu.edu.ua>
2023-12-07 00:57:55 +0200

    1

--- app/modal.tsx
+++ app/modal.tsx
@@ -9,7 +9,7 @@ export default function ModalScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Modal</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text style={styles.text}>This is Pomodoro Timer by Chaika Valeriia. 
+      <Text style={styles.text}>This is Pomodoro Timer by Savina Elithabed. 
       Thanks for downloading this!</Text>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
